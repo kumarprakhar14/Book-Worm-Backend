81d7ed931768e0fef6bba389221eaf5b9962bd00
Kumar Prakhar <kumarprakharkp143@gmail.com>
2026-02-01 03:51:02 +0000

    update login response

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -83,7 +83,8 @@ export const loginUser = async (req, res) => {
                 id: user._id,
                 username: user.username,
                 email: user.email,
-                profileImage: user.profileImage
+                profileImage: user.profileImage,
+                createdAt: user.createdAt
             }
         })
     } catch (error) {
